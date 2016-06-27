ce8df656a118f963b945a2056e43caa23142c295
Joe Levine <joe.levine@fjorgedigital.com>
2016-06-27 20:04:54 +0100

    removed unused code.

--- helpers/activate.php
+++ helpers/activate.php
@@ -5,8 +5,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Activate {
 	public static function activate() {
-		// A random date in the past.
-		// 09 May 1972 08:31:53 UTC if you're interested.
-		add_option( LAST_EXPORTED, 74248313 );
+		// Add any databases or default options here.
 	}
 }
