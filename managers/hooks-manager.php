92ef3542632ebec35e18189770d7fc6ce16a3039
Joe Levine <joe.levine@fjorgedigital.com>
2016-06-27 20:10:36 +0100

    added more notes

--- managers/hooks-manager.php
+++ managers/hooks-manager.php
@@ -9,19 +9,19 @@ class HooksManager {
 	}
 
 	public function init_hooks() {
+		// add any hooks here, actions, filters. Anything but cron hooks get added here.
 	}
 
 	public function quash_unused() {
+		// this is for anyone using an IDE. It can be useful for that, but don't put any code in here.
 	}
 
 	public static function debug_action( $var ) {
 		debug_to_browser_console( $var );
-		echo '<pre class="debug">' . print_r( $var ) . '</pre>';
 	}
 
 	public static function debug_filter( $var ) {
 		debug_to_browser_console( $var );
-
 		return $var;
 	}
 }
